29af751d0a551f36e6d73ba2348adf3e813032e8
Rahul Maurya <rkmaurya@tekion.com>
2023-10-16 02:57:22 +0530

    Updated network error type.

--- app/src/main/java/com/rahul/way_myapplication/view/MainActivity.kt
+++ app/src/main/java/com/rahul/way_myapplication/view/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.rahul.way_myapplication.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.rahul.way_myapplication.R
 import com.rahul.way_myapplication.adapter.ProductListAdapter
 import com.rahul.way_myapplication.data.model.ProductListDataClass
 import com.rahul.way_myapplication.data.services.ApiClient
@@ -13,83 +12,75 @@ import com.rahul.way_myapplication.databinding.ActivityMainBinding
 import com.rahul.way_myapplication.repository.Repository
 import com.rahul.way_myapplication.viewmodel.ProductListViewModel
 import com.rahul.way_myapplication.viewmodel.ProductListViewModelFactory
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainActivity : BaseActivity() {
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var productListAdapter: ProductListAdapter
-//    private val productListViewModel: ProductListViewModel by viewModel()
+    private lateinit var productListViewModel: ProductListViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val productListViewModel = ViewModelProvider(
-            this,
-            ProductListViewModelFactory(Repository(ApiClient.apiService))
-        ).get(ProductListViewModel::class.java)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        productListAdapter = ProductListAdapter(applicationContext, ProductListDataClass())
+        productListViewModel = initializeViewModel()
 
+        productListAdapter = ProductListAdapter(this, ProductListDataClass())
         initRecyclerView()
-        productListViewModel.getProductList()
-        productListViewModel.productList.observe(this) {result ->
 
-            result.onSuccess {
-                val productListData = result.getOrNull()
-                productListData?.let {
-                    productListAdapter.refreshRecyclerView(applicationContext, it)
-                    hideLoader(binding)
-                } ?: run {
-                    showToast("Received null product list")
-                }
-            }
-            result.onFailure {
-                val exception = result.exceptionOrNull()
-                exception?.let {
-                    it.message?.let { it1 -> showToast(it1) }
-                } ?: run {
-                    showToast("Unknown error")
-                }
-                hideLoader(binding)
-            }
-        }
+        observeProductList()
 
         binding.swipeRefreshLayout.setOnRefreshListener {
-            onPullToRefresh(productListViewModel)
+            onPullToRefresh()
         }
     }
 
-    override fun onNetworkStatusChanged(isNetworkAvailable: Boolean) {
-        super.onNetworkStatusChanged(isNetworkAvailable)
-        Log.e("MainActivity", "Status: $isNetworkAvailable")
-        hideLoader(binding)
+    private fun initializeViewModel(): ProductListViewModel {
+        return ViewModelProvider(
+            this,
+            ProductListViewModelFactory(Repository(ApiClient.apiService))
+        )[ProductListViewModel::class.java]
+    }
+
+    private fun observeProductList() {
+        productListViewModel.productList.observe(this) { result ->
+            handleProductListResult(result)
+        }
+    }
+
+    private fun handleProductListResult(result: Result<ProductListDataClass>) {
+        result.onSuccess { productListData ->
+            productListAdapter.refreshRecyclerView(this, productListData)
+            hideLoader()
+        }.onFailure { exception ->
+            exception.message?.let { showToast(it) } ?: showToast(getString(R.string.unknown_error))
+            hideLoader()
+        }
     }
 
     private fun initRecyclerView() {
-        binding.recyclerView.apply {
-            layoutManager = LinearLayoutManager(context)
-            adapter = productListAdapter
-        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = productListAdapter
     }
 
-    private fun onPullToRefresh(productListViewModel: ProductListViewModel) {
+    private fun onPullToRefresh() {
         hideRecyclerView()
-        showLoader(binding)
+        showLoader()
         productListViewModel.getProductList()
         binding.swipeRefreshLayout.isRefreshing = false
     }
+
     private fun hideRecyclerView() {
         binding.recyclerView.visibility = View.GONE
     }
-}
-private fun MainActivity.hideLoader(binding: ActivityMainBinding) {
-    lifecycleScope.launch {
-        delay(1000)
-        binding.recyclerView.visibility = View.VISIBLE
+
+    private fun hideLoader() {
         binding.progressBar.visibility = View.GONE
+        binding.recyclerView.visibility = View.VISIBLE
     }
-}
-private fun MainActivity.showLoader(binding: ActivityMainBinding) {
-    binding.progressBar.visibility = View.VISIBLE
+
+    private fun showLoader() {
+        binding.progressBar.visibility = View.VISIBLE
+    }
 }
