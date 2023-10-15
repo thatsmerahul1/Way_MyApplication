5f7fb5cb01b937331e32f7c2b4759b6426658019
Rahul Maurya <rkmaurya@tekion.com>
2023-10-16 03:04:12 +0530

    Removed dead code

--- app/src/main/java/com/rahul/way_myapplication/utils/NetworkUtil.kt
+++ app/src/main/java/com/rahul/way_myapplication/utils/NetworkUtil.kt
@@ -2,34 +2,10 @@ package com.rahul.way_myapplication.utils
 
 import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
 import android.content.IntentFilter
 import android.net.ConnectivityManager
-import android.widget.Toast
-import com.rahul.way_myapplication.R
 
 object NetworkUtil {
-
-
-//        fun isInternetAvailable(context: Context): Boolean {
-//            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
-//
-//            if (connectivityManager != null) {
-//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                    val network = connectivityManager.activeNetwork
-//                    val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
-//                    return networkCapabilities != null &&
-//                            (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
-//                                    networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
-//                                    networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET))
-//                } else {
-//                    val networkInfo = connectivityManager.activeNetworkInfo
-//                    return networkInfo != null && networkInfo.isConnected
-//                }
-//            }
-//            return false
-//        }
-
     fun initNetworkReceiver(context: Context, receiver: BroadcastReceiver) {
         context.registerReceiver(receiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
     }
@@ -41,7 +17,7 @@ object NetworkUtil {
     }
 
     fun unregisterNetworkReceiver(context: Context,  receiver: BroadcastReceiver) {
-        receiver?.let {
+        receiver.let {
             context.unregisterReceiver(it)
         }
     }
